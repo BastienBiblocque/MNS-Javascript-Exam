de2ade46e265c525c08339b972c82fc80d5a71be
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2022-03-06 19:15:38 +0100

    alert banner en cas d'ajout d'item + verif du formulaire

--- client/assets/script/add.js
+++ client/assets/script/add.js
@@ -1,35 +1,46 @@
 let inputName = document.querySelector('#inputName');
 let buttonSendForm = document.querySelector('#buttonSendForm');
+let alert_banner = document.querySelector('#alert_banner');
 
 buttonSendForm.addEventListener('click',post);
 
 function post () {
-    let tmp = {
-        name: inputName.value,
-        pays: 'UK',
-        id: Date.now().toString(),
-    };
+    if (inputName.value.length === 0 ){
+        inputName.classList.add('border-red-600');
+    } else {
+        inputName.classList.remove('border-red-600');
+        let tmp = {
+            name: inputName.value,
+            pays: 'UK',
+            id: Date.now().toString(),
+        };
 
-    let options = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        mode: 'cors',
-        cache: 'default',
-        body: JSON.stringify(tmp)
-    };
+        let options = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+            body: JSON.stringify(tmp)
+        };
 
-    let url = 'data'
+        let url = 'data'
 
-    fetch(url, options)
-        .then((res) => {
-            if(res.ok) {
-                return res.json();
-            }
-        })
-        .then((response) => {
-            console.log(response);
-        })
+        fetch(url, options)
+            .then((res) => {
+                if(res.ok) {
+                    return res.json();
+                }
+            })
+            .then((response) => {
+                console.log(response);
+                alert_banner.classList.remove('hidden');
+                setTimeout(()=>{
+                    alert_banner.classList.add('hidden');
+                }, 2000);
+            })
+    }
+
 }
